a092e15b8bb3a118fdfb842ceeeee9628e4d6b28
Thuy Vi <caovi1131998@gmail.com>
2020-06-08 22:30:41 +0700

    revert database connection config

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-
 return [
 
     /*
@@ -17,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', $dbopts["host"]),
-            'port' => env('DB_PORT', $dbopts["port"]),
-            'database' => env('DB_DATABASE', ltrim($dbopts["path"],'/')),
-            'username' => env('DB_USERNAME', $dbopts["user"]),
-            'password' => env('DB_PASSWORD', $dbopts["pass"]),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
